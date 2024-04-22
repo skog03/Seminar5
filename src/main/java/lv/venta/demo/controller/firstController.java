dca7a74df3b816c02f061217a7bad4b577ab104b
skog03 <aumale.annija@gmail.com>
2024-04-22 11:25:15 +0300

    First commit

---
+++ src/main/java/lv/venta/demo/controller/firstController.java
@@ -0,0 +1,37 @@
+package lv.venta.demo.controller;
+
+
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+@Controller
+public class firstController 
+{
+
+    @GetMapping("/hello")    //localhost:8080/hello
+    public String getHello() {
+        System.out.println("The first controller is working!!");
+        return "hello-page";  //will show hello-page.html file in the browser
+    }
+
+    Random rand = new Random();
+    @GetMapping("/hello/msg")   //localhost:8080/hello/msg
+    public String getHelloMsg(Model model) {
+        System.out.println("Second controller is working");
+        model.addAttribute("mypackage", "Hello from JAVA: " + rand.nextInt(0, 100));
+        return "hello-msg-page"; //will show hello-msg-page.html file in browser
+    }
+    
+    
+}
+
+//TODO
+//create git repo and push
+//create model package as lv.venta.model
+//create new model class Product in this package (id, title, description, price, quantity)
---
+++ src/main/java/lv/venta/demo/lv.venta.model/Product.java
@@ -0,0 +1,9 @@
+package lv.venta.demo;
+
+public class Product {
+    private int id;
+    private String title;
+    private String description;
+    private float price;
+    private int quantity;
+}   
